a41b2219cf7f187e70721b4018fa75da079fdedd
Alex Bertrand <alex.m.bertrand@gmail.com>
2020-12-17 10:57:12 -0500

    setting up basics

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
 function App() {
   return (
-
+    <div className="App">
+      <h1>Placeholder</h1>
+    </div>
   );
 }
 
