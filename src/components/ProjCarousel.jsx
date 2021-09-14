8d215ba3d98cedd30c6e8e3da219490f03aaee11
Alex Bertrand <alex.m.bertrand@gmail.com>
2021-09-14 10:25:44 -0400

    Fixing carousel images and console errors

--- src/components/ProjCarousel.jsx
+++ src/components/ProjCarousel.jsx
@@ -6,35 +6,37 @@ import Carousel from 'react-bootstrap/Carousel';
 import portfolio from '../utils/portfolio';
 
 function ProjCarousel() {
-    console.log(portfolio);
-    return (
-        <Container className="projects">
-            <Carousel>
-                {portfolio.map((info) => {
-                    return (
-                        <Carousel.Item key={info.key}>
-                            <Row className="projCard">
-                                <Card style={{ width: '40rem' }}>
-                                    <Card.Img variant="top" src={info.image} />
-                                    <Card.Body>
-                                        <Card.Title>{info.title}</Card.Title>
-                                        <Card.Text>{info.description}</Card.Text>
-                                        <Card.Text><strong>Tech Used:</strong> {info.tech}</Card.Text>
-                                        <Card.Link className="projLink" href={info.deployed} target="_blank">
-                                            Deployed App
-                                            </Card.Link>
-                                        <Card.Link className="projLink" href={info.github} target="_blank">
-                                            View Code
-                                            </Card.Link>
-                                    </Card.Body>
-                                </Card>
-                            </Row>
-                        </Carousel.Item>
-                    )
-                })}
-            </Carousel>
-        </Container>
-    )
+  // console.log(portfolio);
+  return (
+    <Container className='projects'>
+      <Carousel>
+        {portfolio.map((info) => {
+          return (
+            <Carousel.Item key={info.key}>
+              <Row className='projCard'>
+                <Card style={{ width: '40rem' }}>
+                  <Card.Img variant='top' src={info.image} />
+                  <Card.Body>
+                    <Card.Title>{info.title}</Card.Title>
+                    <Card.Text>{info.description}</Card.Text>
+                    <Card.Text>
+                      <strong>Tech Used:</strong> {info.tech}
+                    </Card.Text>
+                    <Card.Link className='projLink' href={info.deployed} target='_blank'>
+                      Deployed App
+                    </Card.Link>
+                    <Card.Link className='projLink' href={info.github} target='_blank'>
+                      View Code
+                    </Card.Link>
+                  </Card.Body>
+                </Card>
+              </Row>
+            </Carousel.Item>
+          );
+        })}
+      </Carousel>
+    </Container>
+  );
 }
 
 export default ProjCarousel;
--- src/utils/portfolio.js
+++ src/utils/portfolio.js
@@ -1,64 +1,66 @@
 const portfolio = [
-    {
-        "id": 1,
-        "key": 1,
-        "image": "https://i.ibb.co/6ZkB1JC/while-youre-there.png",
-        "title": "While You're There",
-        "description": "Have you ever gone shopping for someone and forgot something?  We definitely have!  Now with Covid-19 it is more important than ever to remember shopping items in one trip.  Which is exactly what brought us to the idea for While You're There.",
-        "tech": "React, React-Bootstrap, Auth0, FontAwesome, Socket.io, NodeJS, MySQL, Sequelize, Express, nodemon, Balsamiq, Trello",
-        "github": "https://github.com/ambertrand/While-Youre-There",
-        "deployed": "https://while-youre-there.herokuapp.com/"
-    },
-    {
-        "id": 2,
-        "key": 2,
-        "image": "https://i.ibb.co/hXnv8nm/outdoor-adventure-buddies.png",
-        "title": "Outdoor Adventure Buddies",
-        "description": "Throughout these unprecedented times one thing has remained clear, people want to get out and explore the outdoors! For these exact moments we created Outdoor Adventure Buddies.",
-        "tech": "NodeJS, Express, Express-Handlebars, Bootstrap, Passport, bcrypt, MySQL, Sequelize, Hiking Project API, SendGrid, nodemon, dotenv, ESLint",
-        "github": "https://github.com/dylanbest15/Outdoor-Adventure-Buddies",
-        "deployed": "https://whispering-castle-57159.herokuapp.com/"
-    },
-    {
-        "id": 3,
-        "key": 3,
-        "image": "https://media.giphy.com/media/Vh9PHybWF4VsakZqi2/giphy.gif",
-        "title": "MyCovidTracker",
-        "description": "Your one stop shop for all news and statistics on COVID-19!",
-        "tech": "HTML, CSS, Javascript, JQuery, Materialize, Coronoavirus Covid19 API, NY Times API",
-        "github": "https://github.com/cjlaflamme1/MyCovidTracker",
-        "deployed": "https://cjlaflamme1.github.io/MyCovidTracker/"
-    },
-    {
-        "id": 4,
-        "key": 4,
-        "image": "https://i.ibb.co/MVxGxT8/devour-the-burger.png",
-        "title": "Devour The Burger",
-        "description": "A fun application to track burgers as they are logged and eaten using MySQL, Node.js, Express.js & Handlebars.",
-        "tech": "Javascript, NodeJS, Express, Express-Handlebars, MySQL",
-        "github": "https://github.com/ambertrand/Devour-The-Burger",
-        "deployed": "https://gentle-wildwood-15676.herokuapp.com/"
-    },
-    {
-        "id": 5,
-        "key": 5,
-        "image": "https://i.ibb.co/hYvWwGD/weather-dashboard.png",
-        "title": "Weather Dashboard",
-        "description": "A weather dashboard that allows users to search for cities and view a current and 5-day forecast with data from the OpenWeather API.",
-        "tech": "HTML, CSS, Javascript, Bootstrap, JQuery, Moment.js, Openweather API",
-        "github": "https://github.com/ambertrand/Weather-Dashboard",
-        "deployed": "https://ambertrand.github.io/Weather-Dashboard/"
-    },
-    {
-        "id": 6,
-        "key": 6,
-        "image": "https://i.ibb.co/zQqkcsd/password-generator.png",
-        "title": "Password Generator",
-        "description": "A random password generator that allows users to input desired length and character choices. Upon hitting generate password button the desired password will be displayed based on the criteria selected.",
-        "tech": "HTML, CSS, Javascript, Bootstrap, JQuery",
-        "github": "https://github.com/ambertrand/Password-Generator",
-        "deployed": "https://ambertrand.github.io/Password-Generator/"
-    }
-]
+  {
+    id: 1,
+    key: 1,
+    image: 'https://i.imgur.com/MMxvj1s.png',
+    title: "While You're There",
+    description:
+      "Have you ever gone shopping for someone and forgot something?  We definitely have!  Now with Covid-19 it is more important than ever to remember shopping items in one trip.  Which is exactly what brought us to the idea for While You're There.",
+    tech: 'React, React-Bootstrap, Auth0, FontAwesome, Socket.io, NodeJS, MySQL, Sequelize, Express, nodemon, Balsamiq, Trello',
+    github: 'https://github.com/ambertrand/While-Youre-There',
+    deployed: 'https://while-youre-there.herokuapp.com/',
+  },
+  {
+    id: 2,
+    key: 2,
+    image: 'https://i.imgur.com/6Vp6aLy.png',
+    title: 'Outdoor Adventure Buddies',
+    description: 'Throughout these unprecedented times one thing has remained clear, people want to get out and explore the outdoors! For these exact moments we created Outdoor Adventure Buddies.',
+    tech: 'NodeJS, Express, Express-Handlebars, Bootstrap, Passport, bcrypt, MySQL, Sequelize, Hiking Project API, SendGrid, nodemon, dotenv, ESLint',
+    github: 'https://github.com/dylanbest15/Outdoor-Adventure-Buddies',
+    deployed: 'https://whispering-castle-57159.herokuapp.com/',
+  },
+  {
+    id: 3,
+    key: 3,
+    image: 'https://media.giphy.com/media/Vh9PHybWF4VsakZqi2/giphy.gif',
+    title: 'MyCovidTracker',
+    description: 'Your one stop shop for all news and statistics on COVID-19!',
+    tech: 'HTML, CSS, Javascript, JQuery, Materialize, Coronoavirus Covid19 API, NY Times API',
+    github: 'https://github.com/cjlaflamme1/MyCovidTracker',
+    deployed: 'https://cjlaflamme1.github.io/MyCovidTracker/',
+  },
+  {
+    id: 4,
+    key: 4,
+    image: 'https://i.imgur.com/jmUkGNr.png',
+    title: 'Devour The Burger',
+    description: 'A fun application to track burgers as they are logged and eaten using MySQL, Node.js, Express.js & Handlebars.',
+    tech: 'Javascript, NodeJS, Express, Express-Handlebars, MySQL',
+    github: 'https://github.com/ambertrand/Devour-The-Burger',
+    deployed: 'https://gentle-wildwood-15676.herokuapp.com/',
+  },
+  {
+    id: 5,
+    key: 5,
+    image: 'https://i.imgur.com/liWvzNJ.png',
+    title: 'Weather Dashboard',
+    description: 'A weather dashboard that allows users to search for cities and view a current and 5-day forecast with data from the OpenWeather API.',
+    tech: 'HTML, CSS, Javascript, Bootstrap, JQuery, Moment.js, Openweather API',
+    github: 'https://github.com/ambertrand/Weather-Dashboard',
+    deployed: 'https://ambertrand.github.io/Weather-Dashboard/',
+  },
+  {
+    id: 6,
+    key: 6,
+    image: 'https://i.imgur.com/Lpo0upY.png',
+    title: 'Password Generator',
+    description:
+      'A random password generator that allows users to input desired length and character choices. Upon hitting generate password button the desired password will be displayed based on the criteria selected.',
+    tech: 'HTML, CSS, Javascript, Bootstrap, JQuery',
+    github: 'https://github.com/ambertrand/Password-Generator',
+    deployed: 'https://ambertrand.github.io/Password-Generator/',
+  },
+];
 
 export default portfolio;
