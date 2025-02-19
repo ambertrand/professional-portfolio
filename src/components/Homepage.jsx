88e84bb43800dbef3bbb52757da47c313a4be87c
ambertrand <alex.m.bertrand@gmail.com>
2025-02-18 21:07:31 -0500

    porfolito updates 2025

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -7,11 +7,11 @@ function HomePage() {
         <h1 className="name">Alex Bertrand</h1>
         <p className="description">
           I'm a Full Stack Engineer that can create responsive, mobile-first
-          applications with stunning user experiences. Currently I'm a Front-End
-          Engineer focused on building user-friendly & accessible websites
+          applications with stunning user experiences. Currently I'm a Software
+          Engineer focused on modernizing and securing web & mobile applications 
           at&nbsp;
-          <a href="https://www.silvertech.com/" className="company">
-            SilverTech
+          <a href="https://www.theatomgroup.com/" className="company">
+            The ATOM Group
           </a>
         </p>
       </section>
